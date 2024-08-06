716a5b8ecfe9e51776e4b2efa30aa8a94b7b6c94
Thomas Latter <latter.tc@gmail.com>
2024-08-06 18:04:47 +0200

    fix: carousel photo paths

--- src/Pages/Home/Banner/Carousel/Carousel.jsx
+++ src/Pages/Home/Banner/Carousel/Carousel.jsx
@@ -1,83 +1,92 @@
-import {Swiper, SwiperSlide} from "swiper/react";
+import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, EffectCoverflow } from "swiper/modules";
 import "swiper/css";
 import './Carousel.css';
 
+// Import images
+import img1 from '../../assets/photos/carousel/1.jpg';
+import img2 from '../../assets/photos/carousel/2.jpg';
+import img3 from '../../assets/photos/carousel/3.jpg';
+import img4 from '../../assets/photos/carousel/4.jpg';
+import img5 from '../../assets/photos/carousel/5.jpg';
+import img6 from '../../assets/photos/carousel/6.jpg';
+import img7 from '../../assets/photos/carousel/7.jpg';
+
 const Carousel = () => {
-    return (
-      <>
-        <div className="container">
-          <Swiper
-            spaceBetween={30}
-            autoplay={{ delay: 2300, disableOnInteraction: false }}
-            speed={800}
-            effect={"coverflow"}
-            grabCursor={true}
-            centeredSlides={true}
-            loop={true}
-            slidesPerView={"auto"}
-            coverflowEffect={{
-              rotate: 0,
-              stretch: 0,
-              depth: 100,
-              modifier: 2.5,
-            }}
-            modules={[Autoplay, EffectCoverflow]}
-          >
-            <SwiperSlide>
-              <img
-                loading="lazy"
-                src="/src/assets/photos/carousel/1.jpg"
-                alt="football"
-                className="relative"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                loading="lazy"
-                src="/src/assets/photos/carousel/2.jpg"
-                alt="cricket"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                loading="lazy"
-                src="/src/assets/photos/carousel/3.jpg"
-                alt="tennis"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                loading="lazy"
-                src="/src/assets/photos/carousel/4.jpg"
-                alt="badminton"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                loading="lazy"
-                src="/src/assets/photos/carousel/5.jpg"
-                alt="rugby"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                loading="lazy"
-                src="/src/assets/photos/carousel/6.jpg"
-                alt="baseball"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                loading="lazy"
-                src="/src/assets/photos/carousel/7.jpg"
-                alt="baseball"
-              />
-            </SwiperSlide>
-          </Swiper>
-        </div>
-      </>
-    );
+  return (
+    <>
+      <div className="container">
+        <Swiper
+          spaceBetween={30}
+          autoplay={{ delay: 2300, disableOnInteraction: false }}
+          speed={800}
+          effect={"coverflow"}
+          grabCursor={true}
+          centeredSlides={true}
+          loop={true}
+          slidesPerView={"auto"}
+          coverflowEffect={{
+            rotate: 0,
+            stretch: 0,
+            depth: 100,
+            modifier: 2.5,
+          }}
+          modules={[Autoplay, EffectCoverflow]}
+        >
+          <SwiperSlide>
+            <img
+              loading="lazy"
+              src={img1}
+              alt="football"
+              className="relative"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              loading="lazy"
+              src={img2}
+              alt="cricket"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              loading="lazy"
+              src={img3}
+              alt="tennis"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              loading="lazy"
+              src={img4}
+              alt="badminton"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              loading="lazy"
+              src={img5}
+              alt="rugby"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              loading="lazy"
+              src={img6}
+              alt="baseball"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              loading="lazy"
+              src={img7}
+              alt="baseball"
+            />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </>
+  );
 };
 
 export default Carousel;
